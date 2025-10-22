bb7d758744113f88cd1d5cc8ef36d4017231451c
ZaheerAli12134 <zaheermirali121@gmail.com>
2025-10-22 14:05:20 +0100

    fix

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ FRONTEND_URL = os.environ.get("FRONTEND_URL", "http://localhost:3000")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[FRONTEND_URL],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
